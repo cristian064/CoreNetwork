3899b3f932a11c1c12b1297e9befaf12069cf5bd
cristian <cristian.ayala.064@gmail.com>
2022-01-05 19:14:32 -0500

    add protocol methods

--- Sources/CoreNetwork/RestAPI.swift
+++ Sources/CoreNetwork/RestAPI.swift
@@ -9,4 +9,12 @@ import GenericUtilities
 
 public protocol RestAPI {
     
+    func request<ResponseCodable, QueryParam>(with requestUrl: String,
+                                  queryParam: QueryParam,
+                                  completion: @escaping (ResponseAPI<ResponseCodable>) -> Void) where ResponseCodable: Codable, QueryParam: QueryParamEncodable
+    
+    func request<ResponseCodable, ParamBody>(with requestUrl: String,
+                                             httpMethod: HttpVerb,
+                                             paramBody: ParamBody,
+                                             completion: @escaping (ResponseAPI<ResponseCodable>) -> Void) where ResponseCodable: Codable, ParamBody: Codable
 }
